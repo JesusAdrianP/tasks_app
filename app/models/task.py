c522e764f2cefd610095f298230c8708ef2caa33
Jesus <jesus.guetio@correounivalle.edu.co>
2025-12-29 22:46:32 -0500

    adding index on created_at attribute on task table

--- app/models/task.py
+++ app/models/task.py
@@ -15,10 +15,10 @@ class Task(Base):
     __tablename__ = "task"
     
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String(255), index=True, nullable=False)
+    title = Column(String(255), nullable=False)
     description = Column(Text, nullable=False)
     status = Column(Enum(TaskStatus, name="task_status"), default=TaskStatus.pending, index=True, nullable=False)
-    created_at = Column(DateTime(timezone=True), default=lambda: datetime.now(tz=timezone.utc), nullable=False)
+    created_at = Column(DateTime(timezone=True), default=lambda: datetime.now(tz=timezone.utc), index=True,nullable=False)
     updated_at = Column(DateTime(timezone=True), default=lambda: datetime.now(tz=timezone.utc), onupdate=lambda: datetime.now(tz=timezone.utc), nullable=False)
     created_by = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), index=True, nullable=False)
     
